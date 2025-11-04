efe32b7a9ba85423b8531f0e8e8fb34dad0bf9d8
aUser-12 <brahmandamakhil@gmail.com>
2025-11-05 01:24:00 +0530

    New initial commit lmao

--- Formula/maca.rb
+++ Formula/maca.rb
@@ -3,8 +3,8 @@ class Maca < Formula
 
   desc "A feature complete mac address changer for macOS"
   homepage "https://github.com/aUser-12/homebrew-maca"
-  url "https://github.com/aUser-12/homebrew-maca/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "5e7f0ae1115f0040d04033673c9ace202bb7aedd15070d291984ac1d919ed410"
+  url "https://github.com/aUser-12/homebrew-maca/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "9cd3eba88e81e101be8fcb2fd7e1a4c8f436c54e3b0b79a1f54d87bc82809a28"
   license "MIT"
 
   depends_on "python"
